864e462e4f829988fea66b8dd838546e762411fb
Doctorwu <wuzhouchun@qq.com>
2021-06-22 12:07:09 +0800

    feat: clear JS

--- JS/docs/tokenizer.js
+++
@@ -1,5 +0,0 @@
-"use strict";
-var esprima = require('esprima');
-var sourceCode = "<h1 id=\"title\"><span>hello</span>world</h1>";
-var ast = esprima.parseModule(sourceCode, { jsx: true, tokens: true });
-console.log(ast);
